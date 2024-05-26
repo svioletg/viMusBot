47718d5689ddc15668d0122693e43b28470c97ff
Seth "Violet" Gibbs <16344364+svioletg@users.noreply.github.com>
2024-05-26 14:24:14 -0500

    Add -3 to py command in setup bat to use latest version

--- envsetup.bat
+++ envsetup.bat
@@ -1,8 +1,8 @@
 @echo off
 
-echo (py -m venv .vmbvenv) Creating virtual enviornment...
+echo (py -3 -m venv .vmbvenv) Creating virtual enviornment...
 
-py -m venv .vmbvenv
+py -3 -m venv .vmbvenv
 
 echo (.vmbvenv\scripts\python -m pip install -r requirements.txt) Installing required packages...
 
